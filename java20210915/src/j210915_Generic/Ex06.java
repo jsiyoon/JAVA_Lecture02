16c0b258283f0b59986a0a86f08aa44e92451437
Jsiyoon <jjc9090@naver.com>
2021-09-27 14:29:28 +0900

    Generic

---
+++ java20210915/src/j210915_Generic/Ex06.java
@@ -0,0 +1,17 @@
+package j210915_Generic;
+
+public class Ex06 {
+
+}
+class MyType<T>{
+	
+}
+class ChildType<T> extends MyType<T>{
+	
+}
+class Child2Type<T, U> extends MyType<T>{
+	
+}
+class Child3Type extends MyType<String>{
+	
+}
