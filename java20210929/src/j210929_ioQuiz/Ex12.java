27b4c0e97686b0bb00bb88256a0fb474c301feb3
Jsiyoon <jjc9090@naver.com>
2021-09-30 16:01:20 +0900

    BufferedReader_Quiz

--- java20210929/src/j210929_ioQuiz/Ex12.java
+++ java20210929/src/j210929_ioQuiz/Ex12.java
@@ -2,12 +2,11 @@ package j210929_ioQuiz;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.Reader;
 
 public class Ex12 {
 	public static void main(String[] args) throws Exception{
 		String file = "src/j210929_ioQuiz/Ex12.java";
-		Reader reader = new FileReader(file);
+		FileReader reader = new FileReader(file);
 		BufferedReader br = new BufferedReader(reader);
 		
 		String line = br.readLine();
